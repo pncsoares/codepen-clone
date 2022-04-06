910feb5be1fb5f15f682c83508b06efa77450cd5
Pedro Soares <pncsoares@gmail.com>
2022-04-06 16:33:38 +0100

    Create codepen algorithm and UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor';
+import useLocalStorage from '../hooks/useLocalStorage';
+
 function App() {
-  return 'Hello World!'
+    const [html, setHtml] = useLocalStorage('html', '');
+    const [css, setCss] = useLocalStorage('css', '');
+    const [javascript, setJavascript] = useLocalStorage('javascript', '');
+    const [srcDoc, setSrcDoc] = useState('');
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setSrcDoc(`
+                <html>
+                    <body>${html}</body>
+                    <style>${css}</style>
+                    <script>${javascript}</script>
+                </html>
+            `);
+        }, 250);
+
+        return () => clearTimeout(timeout);
+    }, [html, css, javascript]);
+
+    return (
+        <>
+            <div className="pane top-pane">
+                <Editor
+                    language="xml"
+                    displayName="HTML"
+                    value={html}
+                    onChange={setHtml}
+                />
+                <Editor
+                    language="css"
+                    displayName="CSS"
+                    value={css}
+                    onChange={setCss}
+                />
+                <Editor
+                    language="javascript"
+                    displayName="JavaScript"
+                    value={javascript}
+                    onChange={setJavascript}
+                />
+            </div>
+            <div className="pane">
+                <iframe
+                    srcDoc={srcDoc}
+                    title="output"
+                    sandbox="allow-scripts"
+                    frameBorder="0"
+                    width="100%"
+                    height="100%"
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
